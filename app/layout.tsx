474c42062f22beed546c486fd48bed125769f18f
atomsiwawut1 <atomsiwawut@gmail.com>
2024-09-03 00:00:13 +0700

    “Update“

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Nunito, Josefin_Sans } from 'next/font/google';
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import Footer from "./components/footer";
+import Header from "./components/header";
+
+
+
+const inter = Nunito({ subsets: ['latin'] });
+const cuteFont = Josefin_Sans({ subsets: ['latin'], weight: '400' });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +21,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+
+      <html lang="en">
+      <body className={inter.className}>
+        <Header font={cuteFont.className} />
+        <hr className="h-0.5 bg-gray-100 border-0"></hr>
+        <div className="bg-gray-951 py-12">{children}</div>
+        <Footer font={cuteFont.className} />
+      </body>
+      </html>
+
+    </>
+
   );
 }
---
+++ app/products/layout.tsx
@@ -0,0 +1,19 @@
+import Footer from "../components/footer";
+import Header from "../components/header";
+
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return <>
+
+        <div>{children}</div>
+    </>
+
+
+
+
+        ;
+}
